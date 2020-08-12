8bdabcd139eeaba7ffd021ad58dc29c2a3678ecf
Elijah Kizito <elijahknsubuga@gmail.com>
2020-08-12 21:50:35 +0300

    last commit

--- .my_commands.sh
+++ .my_commands.sh
@@ -3,7 +3,16 @@
 function create(){
 
     cd
-    python create.py $1
-    cd /home/elabs/Downloads/myProjects
+    python3 create.py $1
+    cd /home/elabs/Downloads/myProjects/$1
+    git init
+    git remote add origin git@github.com:iCodeElijah/$1.git
+    touch README.md
+    git add .
+    git commit -m"First Commit"
+    git push -u origin master
+    code .
 
 }
+
+# source ~/.my_commands.sh
--- create.py
+++ create.py
@@ -8,10 +8,20 @@ browser.get('https://github.com/login')
 
 def create():
     folderName = str(sys.argv[1])
-    os.maked(path + folderName)
-    python_button = browser.find_element_by_xpath("//*[@id='login_field']")[0]
+    #os.maked(path + folderName)
+    python_button = browser.find_elements_by_xpath("//input[@name='login']")[0]
     python_button.send_keys('iCodeElijah')
-    
+    python_button = browser.find_elements_by_xpath("//input[@name='password']")[0]
+    python_button.send_key('Elijahk.56092')
+    python_button = browser.find_elements_by_xpath("//input[@name='commit']")[0]
+    python_button.click()
+    browser.get('https://github.com/new')
+    python_button = browser.find_elements_by_xpath("//input[@name='repository[name]']")[0]
+    python_button.send_keys(folderName)
+    python_button = browser.find_elements_by_css_selector('button.first-in-line')
+    python_button.submit()
+    browser.quit()
+    #python_button.click()
 if __name__ == "__main__":
     create()
     
